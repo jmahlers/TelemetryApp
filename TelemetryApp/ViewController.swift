6eef81add529661fd5e0eb576afefe81c8c83197
Jeff Ahlers <jahlers@wustl.edu>
2019-10-30 16:49:56 -0500

    Initial Commit

---
+++ TelemetryApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TelemetryApp
+//
+//  Created by Jeff Ahlers on 10/30/19.
+//  Copyright © 2019 Jeff Ahlers. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
