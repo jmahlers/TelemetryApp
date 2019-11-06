a989775186b535f27d1ec9d6ad752f020b676771
Jeff Ahlers <jahlers@wustl.edu>
2019-11-06 01:21:39 -0600

    Attempted to get livestream to work better. It didn't work

--- TelemetryApp/LivestreamView.swift
+++ TelemetryApp/LivestreamView.swift
@@ -9,22 +9,22 @@
 import UIKit
 import WebKit
 
-class LivestreamView: UIViewController, TelemetryDelegate {
+class LivestreamView: UIViewController, TelemetryDelegate, UIScrollViewDelegate{
     
     @IBOutlet weak var livestream: WKWebView!
     
     override func viewWillAppear(_ animated: Bool) {
-            Telemetry.shared.delegate = self
+        Telemetry.shared.delegate = self
+        livestream.scrollView.isScrollEnabled=false
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        livestream.scrollView.contentInset = .zero
-        livestream.autoresizingMask = [.flexibleWidth, .flexibleHeight]
         //Using pokemon as placeholder for now
         //I have absolutely no fucking idea why the 1.17 factor works but it does
-        let iframe = "<iframe src=\"https://player.twitch.tv/?channel=twitchplayspokemon\" frameborder=\"0\" allowfullscreen=\"true\" scrolling=\"no\" height=\"\(livestream.frame.height*UIScreen.main.scale)\" width=\"\(livestream.frame.width*UIScreen.main.scale*1.17)\" playsinline=\"1\"></iframe>"
+        let iframe = "<iframe src=\"https://player.twitch.tv/?channel=twitchplayspokemon\" frameborder=\"0\" allowfullscreen=\"true\" scrolling=\"no\" height=\"\(livestream.frame.height*UIScreen.main.scale)\" width=\"\(livestream.frame.width*UIScreen.main.scale)\"></iframe>"
         livestream.loadHTMLString(iframe, baseURL: nil)
+        livestream.scrollView.backgroundColor = UIColor.black
     }
     
     
