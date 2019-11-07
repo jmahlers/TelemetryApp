14d72f23681df3bbbd704745436ba567c3fcd961
Jeff Ahlers <jahlers@wustl.edu>
2019-11-06 18:19:18 -0600

    Added more dock logic. Beginning animation

--- TelemetryApp/Telemetry/ContainerController.swift
+++ TelemetryApp/Telemetry/ContainerController.swift
@@ -22,11 +22,35 @@ class ContainerController: UIViewController {
         DockView.isUserInteractionEnabled = true
         DockView.addGestureRecognizer(panGesture)
     }
-
+    
     @objc func draggedView(_ sender:UIPanGestureRecognizer){
-        self.view.bringSubviewToFront(DockView)
-        let translation = sender.translation(in: self.view)
-        DockHeight.constant -= translation.y
-        sender.setTranslation(CGPoint.zero, in: self.view)
+        switch sender.state{
+        case .changed:
+        if(DockHeight.constant >= self.view.bounds.height*0.7){
+            let translation = sender.translation(in: self.view)
+            if(translation.y > 0){
+                DockHeight.constant -= translation.y
+            }
+            sender.setTranslation(CGPoint.zero, in: self.view)
+        }else{
+            let translation = sender.translation(in: self.view)
+            DockHeight.constant -= translation.y
+            
+            if(DockHeight.constant >= self.view.bounds.height*0.7){
+                DockHeight.constant = self.view.bounds.height*0.7
+            }
+            sender.setTranslation(CGPoint.zero, in: self.view)
+        }
+            break
+        case .ended:
+            self.DockHeight.constant = 0
+            UIView.animate(withDuration: 0.35, delay: 0.0, options: .curveEaseIn, animations: {
+                self.view.layoutIfNeeded()
+            }, completion: nil)
+            break
+        default:
+            break
+        }
     }
+    
 }
