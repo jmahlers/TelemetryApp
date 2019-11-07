426982c16b5111483c373e32be9a1ccd3fe510b2
Jake Kendrick <jake.kendrick@gmail.com>
2019-11-07 15:09:13 -0600

    Changes to LineChartViewController

--- TelemetryApp/Graph Example/LineChartViewController.swift
+++ TelemetryApp/Graph Example/LineChartViewController.swift
@@ -9,17 +9,34 @@
 import UIKit
 import Charts
 
-class LineChartViewController: BaseChartViewController {
+class LineChartViewController: BaseChartViewController, TelemetryDelegate {
+    let frequency:Double = 4
     
+    func manageMessage(_ event: SensorReading) {
+        let sensor = Sensor(event)
+        if (sensor.key == "FrontLeftDamperTravel") {
+            print(event)
+//            chartView.lineData?.addEntry(<#T##e: ChartDataEntry##ChartDataEntry#>, dataSetIndex: <#T##Int#>)
+//            chartView.data?.addEntry(<#T##e: ChartDataEntry##ChartDataEntry#>, dataSetIndex: <#T##Int#>)
+            updateChartData()
+        }
+    }
+    
+    func manageOpen() {
+        
+    }
+    
+    func manageComplete() {
+        
+    }
     
     @IBOutlet weak var chartView: LineChartView!
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // Do any additional setup after loading the view.
-        self.title = "Line Chart 1"
+        self.title = "FrontLeftDamperTravel"
         self.options = [.toggleValues,
                         .toggleFilled,
                         .toggleCircles,
@@ -53,23 +70,9 @@ class LineChartViewController: BaseChartViewController {
         chartView.xAxis.gridLineDashLengths = [10, 10]
         chartView.xAxis.gridLineDashPhase = 0
         
-        let ll1 = ChartLimitLine(limit: 150, label: "Upper Limit")
-        ll1.lineWidth = 4
-        ll1.lineDashLengths = [5, 5]
-        ll1.labelPosition = .topRight
-        ll1.valueFont = .systemFont(ofSize: 10)
-        
-        let ll2 = ChartLimitLine(limit: -30, label: "Lower Limit")
-        ll2.lineWidth = 4
-        ll2.lineDashLengths = [5,5]
-        ll2.labelPosition = .bottomRight
-        ll2.valueFont = .systemFont(ofSize: 10)
-        
         let leftAxis = chartView.leftAxis
         leftAxis.removeAllLimitLines()
-        leftAxis.addLimitLine(ll1)
-        leftAxis.addLimitLine(ll2)
-        leftAxis.axisMaximum = 200
+        leftAxis.axisMaximum = 100
         leftAxis.axisMinimum = -50
         leftAxis.gridLineDashLengths = [5, 5]
         leftAxis.drawLimitLinesBehindDataEnabled = true
@@ -79,19 +82,19 @@ class LineChartViewController: BaseChartViewController {
         //[_chartView.viewPortHandler setMaximumScaleY: 2.f];
         //[_chartView.viewPortHandler setMaximumScaleX: 2.f];
         
-        let marker = BalloonMarker(color: UIColor(white: 180/255, alpha: 1),
-                                   font: .systemFont(ofSize: 12),
-                                   textColor: .white,
-                                   insets: UIEdgeInsets(top: 8, left: 8, bottom: 20, right: 8))
-        marker.chartView = chartView
-        marker.minimumSize = CGSize(width: 80, height: 40)
-        chartView.marker = marker
+//        let marker = BalloonMarker(color: UIColor(white: 180/255, alpha: 1),
+//                                   font: .systemFont(ofSize: 12),
+//                                   textColor: .white,
+//                                   insets: UIEdgeInsets(top: 8, left: 8, bottom: 20, right: 8))
+//        marker.chartView = chartView
+//        marker.minimumSize = CGSize(width: 80, height: 40)
+//        chartView.marker = marker
         
-        chartView.legend.form = .line
-        
-        slidersValueChanged(nil)
+//        chartView.legend.form = .line
         
         chartView.animate(xAxisDuration: 2.5)
+        
+        Telemetry.shared.delegate = self
     }
     
     override func updateChartData() {
@@ -104,20 +107,33 @@ class LineChartViewController: BaseChartViewController {
     }
     
     func setDataCount(_ count: Int, range: UInt32) {
-        let values = (0..<count).map { (i) -> ChartDataEntry in
-            let val = Double(arc4random_uniform(range) + 3)
-            return ChartDataEntry(x: Double(i), y: val, icon: nil)
+        guard let data = Telemetry.shared.dataSource[Sensor(key: "FrontLeftDamperTravel", unit: nil, description: nil, system: nil)] else {
+            print("data couldn't be accessed from telemetry data source (it probably doesn't exist)")
+            return
+        }
+            
+        let numSeconds = 30
+        let dataFromLastNSecondsSlice = data.suffix(Int(frequency)*numSeconds)
+        let dataFromLastNSeconds = Array(dataFromLastNSecondsSlice)
+        
+        var entries:[ChartDataEntry] = []
+        
+        for datum in dataFromLastNSeconds {
+            let entry:ChartDataEntry = ChartDataEntry(x: 0, y: Double(datum))
+            entries.append(entry)
         }
         
-        let set1 = LineChartDataSet(entries: values, label: "DataSet 1")
+        
+        let set1 = LineChartDataSet(entries: entries, label: "FrontLeftDamperTravel")
         set1.drawIconsEnabled = false
         
-        set1.lineDashLengths = [5, 2.5]
+        set1.lineDashLengths = [5, 0]
         set1.highlightLineDashLengths = [5, 2.5]
         set1.setColor(.black)
-        set1.setCircleColor(.black)
+        //set1.setCircleColor(.black)
         set1.lineWidth = 1
-        set1.circleRadius = 3
+        set1.circleRadius = 0
+        set1.drawValuesEnabled = false
         set1.drawCircleHoleEnabled = false
         set1.valueFont = .systemFont(ofSize: 9)
         set1.formLineDashLengths = [5, 2.5]
@@ -132,9 +148,9 @@ class LineChartViewController: BaseChartViewController {
         set1.fill = Fill(linearGradient: gradient, angle: 90) //.linearGradient(gradient, angle: 90)
         set1.drawFilledEnabled = true
         
-        let data = LineChartData(dataSet: set1)
+        let chartData = LineChartData(dataSet: set1)
         
-        chartView.data = data
+        chartView.data = chartData
     }
     
     override func optionTapped(_ option: Option) {
@@ -173,9 +189,5 @@ class LineChartViewController: BaseChartViewController {
             super.handleOption(option, forChartView: chartView)
         }
     }
-    
-    @IBAction func slidersValueChanged(_ sender: Any?) {
-        
-        self.updateChartData()
-    }
+
 }
