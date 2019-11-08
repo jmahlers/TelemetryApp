5a54348985c3261c4dc1f9120836904f47dbb328
Jeff Ahlers <jahlers@wustl.edu>
2019-11-08 11:18:03 -0600

    Added DraggableDock which extends TelemetryViewController in order to clean up code

--- TelemetryApp/Telemetry Tab/TelemetryViewContoller.swift
+++ TelemetryApp/Telemetry Tab/TelemetryViewContoller.swift
@@ -29,61 +29,8 @@ class TelemetryViewController: UIViewController,UICollectionViewDataSource, UICo
         DockOutlet.roundCorners(cornerRadius: 12.5)
     }
     
-    @objc func draggedView(_ sender:UIPanGestureRecognizer){
-        let inset = view.safeAreaInsets.top + view.safeAreaInsets.bottom
-        let upwardTop = view.frame.height - inset    //Portion of screen to fill
-        let upwardHeight = upwardTop*0.93 - headerHeight //Account for static height multiplier in storyboard
-        
-        switch sender.state{
-        case .began:
-            //DockOutlet.makeOpaque()
-            break
-            
-        case .changed:
-            let translation = sender.translation(in: self.view)
-            if(dockHeight.constant - translation.y >= upwardHeight){
-                dockHeight.constant = upwardHeight
-            }else if(dockHeight.constant - translation.y <= 0){
-                dockHeight.constant = 0
-            }else{
-                dockHeight.constant -= translation.y
-            }
-            sender.setTranslation(CGPoint.zero, in: self.view)
-            break
-            
-        case .ended:
-            if(DockOutlet.bounds.height > upwardTop*0.2 && sender.velocity(in: self.view).y<0){
-                dockHeight.constant = upwardHeight
-                upwardState = true
-                UIView.animate(withDuration: 0.13, delay: 0.0, options: .curveEaseInOut, animations: {
-                    self.view.layoutIfNeeded()
-                }, completion:  {(_) in
-                    self.DockOutlet.expandDock()
-                })
-            }else if(DockOutlet.bounds.height < upwardTop*0.85 && sender.velocity(in: view).y>0){
-                dockHeight.constant = 0
-                upwardState = false
-                UIView.animate(withDuration: 0.13, delay: 0.0, options: .curveEaseInOut, animations: {
-                    self.view.layoutIfNeeded()
-                }, completion:  {(_) in
-                    self.DockOutlet.minimizeDock()
-                })
-            }else{
-                if(upwardState == true){
-                    dockHeight.constant = upwardHeight
-                }else{
-                    dockHeight.constant = 0
-                }
-                UIView.animate(withDuration: 0.1, delay: 0.0, options: .curveEaseIn, animations: {
-                    self.view.layoutIfNeeded()
-                }, completion:  nil)
-            }
-            break
-            
-        default:
-            break
-        }
-    }
+    
+    
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
     return 1
     }
@@ -101,3 +48,5 @@ class TelemetryViewController: UIViewController,UICollectionViewDataSource, UICo
         return size
     }
 }
+
+
