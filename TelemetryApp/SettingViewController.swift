e0b843a8f4f94f03d4f9480d11c2eaf827efc2ee
Jeff Ahlers <jahlers@wustl.edu>
2019-11-08 19:37:30 -0600

    Setting

---
+++ TelemetryApp/SettingViewController.swift
@@ -0,0 +1,14 @@
+//
+//  SettingViewController.swift
+//  TelemetryApp
+//
+//  Created by Jeff Ahlers on 11/8/19.
+//  Copyright © 2019 Jeff Ahlers. All rights reserved.
+//
+
+import UIKit
+class SettingViewController: UIViewController{
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
