a67e74d310fa01e40aa91543dfdf262a67aa7d0f
Jeff Ahlers <jahlers@wustl.edu>
2019-11-11 18:10:12 -0600

    Settings Collection

--- TelemetryApp/Settings Modal/SettingsViewController.swift
+++ TelemetryApp/Settings Modal/SettingsViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class SettingsViewController: UIViewController {
 
+    @IBOutlet weak var settingsCollection: UITableView!
+    
     var height = CGFloat(400)
     var width = CGFloat(300)
     
