9bb0dd230750bbf08931060818e2452b9320f63a
Jeff Ahlers <jahlers@wustl.edu>
2019-11-11 18:35:10 -0600

    Settings Table View

---
+++ TelemetryApp/Settings Modal/SettingsCell.swift
@@ -0,0 +1,26 @@
+//
+//  SettingsCell.swift
+//  TelemetryApp
+//
+//  Created by Jeff Ahlers on 11/11/19.
+//  Copyright © 2019 Jeff Ahlers. All rights reserved.
+//
+
+import UIKit
+
+class SettingsCell: UITableViewCell {
+
+    @IBOutlet weak var settingLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
--- TelemetryApp/Settings Modal/SettingsViewController.swift
+++ TelemetryApp/Settings Modal/SettingsViewController.swift
@@ -8,15 +8,31 @@
 
 import UIKit
 
-class SettingsViewController: UIViewController {
+class SettingsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource{
 
-    @IBOutlet weak var settingsCollection: UITableView!
+
+    @IBOutlet weak var settingsTable: UITableView!
     
-    var height = CGFloat(400)
-    var width = CGFloat(300)
-    
+    var height = CGFloat(350)
+    var width = CGFloat(190)
+    var settings:[String] = ["Bug Report", "Console","Dock Options","Favorites","Settings"]
     override func viewDidLoad() {
         super.viewDidLoad()
+        settingsTable.dataSource = self
+         let cellNib = UINib(nibName: "SettingsCell", bundle: nil)
+        settingsTable.register(cellNib, forCellReuseIdentifier: "SettingsCell")
         // Do any additional setup after loading the view.
     }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return settings.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = settingsTable.dequeueReusableCell(withIdentifier: "SettingsCell", for: indexPath) as! SettingsCell
+        cell.backgroundColor = UIColor.clear
+        cell.settingLabel.text = settings[indexPath.row]
+        return cell
+    }
+    
 }
