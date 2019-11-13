2864679ca6f1b50279a1f74ac7efaaab2bc1e8c9
Jeff Ahlers <jahlers@wustl.edu>
2019-11-13 11:13:39 -0600

    Storyboard somewhat fixed

---
+++ TelemetryApp/Settings Modal/Weather/WeatherViewController.swift
@@ -0,0 +1,30 @@
+//
+//  WeatherViewController.swift
+//  TelemetryApp
+//
+//  Created by Jeff Ahlers on 11/13/19.
+//  Copyright © 2019 Jeff Ahlers. All rights reserved.
+//
+
+import UIKit
+
+class WeatherViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
