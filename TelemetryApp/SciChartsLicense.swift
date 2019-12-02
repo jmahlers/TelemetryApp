c6e5acf946777cf7815209a14db7a9743cd5b86f
Jeff Ahlers <jahlers@wustl.edu>
2019-12-02 14:50:16 -0600

    License correct final

--- TelemetryApp/SciChartsLicense.swift
+++ TelemetryApp/SciChartsLicense.swift
@@ -5,4 +5,4 @@
 //  Created by Jeff Ahlers on 12/2/19.
 //  Copyright © 2019 Jeff Ahlers. All rights reserved.
 //
-//Talk to Jeff or Jake to add the license
+let licence:String = "<LicenseContract>"+"<Customer>Washington University</Customer>"+"<OrderId>EDUCATIONAL-USE-0125</OrderId>"+"<LicenseCount>2</LicenseCount>"+"<IsTrialLicense>false</IsTrialLicense>"+"<SupportExpires>02/12/2020 00:00:00</SupportExpires>"+"<ProductCode>SC-IOS-2D-PRO</ProductCode>"+"<KeyCode>57c8df8f264cd4306dd05bfa783581e50f93f6d81b19f0787bcaec44ccdfdfec93d50e2eb793b5b9946e3873f94f44af328cc3d8e94114b226283dbb30711879c3c695d4a33e7fcfec1d8f7ae2b1224848690afefaae8f3ab79284bc177b92148eb69f72e9af485fb5f4b0dc7632eef62bc6d3f0a34f9122cc505694c071e58e3284ddf02c062d7d1bb9069024e11db4ef2679dc31cecbb8edd1cb1846100f71dd2f8c7d660c6e731d23d6083d70</KeyCode>"+"</LicenseContract>"
