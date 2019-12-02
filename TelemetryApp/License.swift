25dac0e4f1858ad2b89026e4e338c00895ddff9c
Jeff Ahlers <jahlers@wustl.edu>
2019-12-02 14:00:37 -0600

    Proper license format

---
+++ TelemetryApp/License.swift
@@ -0,0 +1,9 @@
+//
+//  License.swift
+//  TelemetryApp
+//
+//  Created by Jeff Ahlers on 12/2/19.
+//  Copyright © 2019 Jeff Ahlers. All rights reserved.
+//
+
+let licence:String = "<LicenseContract>"+"<Customer>Washington University</Customer>"+"<OrderId>EDUCATIONAL-USE-0125</OrderId>"+"<LicenseCount>2</LicenseCount>"+"<IsTrialLicense>false</IsTrialLicense>"+"<SupportExpires>02/12/2020 00:00:00</SupportExpires>"+"<ProductCode>SC-IOS-2D-PRO</ProductCode>"+"<KeyCode>3e1fb899760442100fcf1dadf2cf09fa54b05a19e02de51a9217f4daede19c2e2e507e08ff7938ef9ebb813ae7a21492c6ec29ebd05963e806d603aa7ce2f04eec43401c341c92b538aa104b7f79ce6ecceab51379dde9fb3c7ef5ce56de3e7b4960972a66b019f28bdf9ec1b394513ecab2b9091496e51bf47724ecd189955c10e66b8cbb322fbf940aa6424c3cce05774561696287875b4f1ca854607b75930006dc5f1cceccfdec659bac177f</KeyCode>"+"</LicenseContract>"
